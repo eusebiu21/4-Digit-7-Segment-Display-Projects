1911652ea6c9cbd1350ce83522de73d1a6f0dbbc
eusebiu21 <52781491+eusebiu21@users.noreply.github.com>
2021-10-27 19:38:44 +0300

    Add files via upload

---
+++ segment_display.ino
@@ -0,0 +1,19 @@
+#include "SevSeg.h"
+SevSeg sevseg; 
+
+void setup(){
+  byte numDigits = 4;
+  byte digitPins[] = {10, 11, 12, 13};
+  byte segmentPins[] = {2, 3, 4, 5, 6, 7, 8, 9};
+
+  bool resistorsOnSegments = true; 
+  bool updateWithDelaysIn = true;
+  byte hardwareConfig = COMMON_ANODE; 
+  sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments);
+  sevseg.setBrightness(90);
+}
+
+void loop(){
+    sevseg.setNumber(4444);
+    sevseg.refreshDisplay(); 
+}
